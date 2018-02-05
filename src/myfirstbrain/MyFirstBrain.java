c9c20fa87f2bb90a0a32610be0a2db8b310d583c
Jérémy DUFOUR <jeremydufour@MacBook-Pro-de-Jeremy.local>
2018-02-05 16:11:42 +0100

    Suppression des déchets du main et modification de la fermeture des Tab

--- src/myfirstbrain/MyFirstBrain.java
+++ src/myfirstbrain/MyFirstBrain.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package myfirstbrain;
 
 import Menu.MenuForm;
@@ -17,10 +12,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author stag
- */
 public class MyFirstBrain extends Application {
     
     @Override

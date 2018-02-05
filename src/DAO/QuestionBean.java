42ccbd4937cf6e8e047d295cbda11b4865823840
celineC <caillierez.celine@gmail.com>
2018-02-05 15:57:40 +0100

    création questionBean

---
+++ src/DAO/QuestionBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAO;
+
+/**
+ *
+ * @author $céline
+ */
+public class QuestionBean {
+    
+    int ID;
+    int niveau;
+    String question;
+    String reponse;
+}

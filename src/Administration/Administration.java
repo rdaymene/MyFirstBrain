501af6ae8517a24c9d398af46de355bbf0c822b6
Jérémy DUFOUR <jeremydufour@MacBook-Pro-de-Jeremy.local>
2018-02-08 14:15:30 +0100

    Ajout dans la classe Administration du code qui gère l'affichage

--- src/Administration/Administration.java
+++ src/Administration/Administration.java
@@ -11,61 +11,149 @@ import javafx.scene.layout.VBox;
 
 public class Administration extends VBox{
     
-    public Administration(){
+    private final VBox radiovbox;
+    
+    private final GridPane radioGrid;
+    private final ToggleGroup radioniveauGroup;
 
+    private final Label radioText;
+    private final RadioButton radioniveau1;
+    private final RadioButton radioniveau2;
+    private final GridPane modifieretajoutergrid;
+    
+    private final VBox ajoutervbox ;
+    
+    private final Label labelajouter;
+    
+    private final GridPane questionreponsegridajoutergrid;
+    private final Label question_label_ajouter ;
+    private final TextField questionajoutertext ;
+    private final Label reponse_label_ajouter ;
+    private final TextField reponseajoutertext ;
+    //bouton du panneau modifier
+    private final GridPane annuleretenregisterajoutergrid;
+    private final Button annulerajouter;
+    private final Button enregistrerajouter;
+    
+    
+    
+    private final VBox modifiervbox ;
+    
+    private final Label labelmodifier;
+    
+    private final GridPane questionreponsemodifiergrid;
+    private final Label question_label_modifier ;
+    private final TextField questionmodifiertext ;
+    private final Label reponse_label_modifier ;
+    private final TextField reponsemodifiertext ;
+    //bouton du panneau modifier
+    private final GridPane annuleretenregistrermodifiergrid;
+    private final Button annulermodifier;
+    private final Button enregistrermodifier;
+    
+    private final Button btnsedeconnecter;
+    
+    public Administration(){
+       
         //Choix du Niveau
-        GridPane radioGrid = new GridPane();
-        ToggleGroup radioniveauGroup = new ToggleGroup();
+        radiovbox = new VBox();
+        
+        radioGrid = new GridPane();
+        radioniveauGroup = new ToggleGroup();
              
-        Label radioText = new Label("Niveau de la question à modifier");
-        RadioButton radioniveau1 = new RadioButton("1");
-        RadioButton radioniveau2 = new RadioButton("2");
+        radioText = new Label("Niveau de la question à modifier");
+        radioniveau1 = new RadioButton("1");
+        radioniveau2 = new RadioButton("2");
         
         radioniveau1.setToggleGroup(radioniveauGroup);
         radioniveau2.setToggleGroup(radioniveauGroup);
         
-        radioGrid.add(radioText,1,0);
-        radioGrid.add(radioniveau1,2,0);
-        radioGrid.add(radioniveau2,3,0);
+        radioGrid.add(radioniveau1,0,1);
+        radioGrid.add(radioniveau2,1,1);
+        radioGrid.setHgap(20);
+        radioGrid.setAlignment(Pos.CENTER);
+        
+        radiovbox.getChildren().add(radioText);
+        radiovbox.getChildren().add(radioGrid);
         
         //radioGrid.vgapProperty(20);
-        //Choix modifier et ajouter
-        GridPane modifierajouterGrid = new GridPane();
+//=============================== CONTAINER DU PANNEAU MODIFIER ET AJOUTER ======================================
+        modifieretajoutergrid = new GridPane();
+//=============================== PANNEAU AJOUTER ==============================================================
+        //Construction de l'affichage du panneau qui permet d'ajouter une question
+        ajoutervbox = new VBox();
         
-        Button btnmodifier = new Button("Modifier");
-        Button btnajouter = new Button("Ajouter");
-        modifierajouterGrid.add(btnmodifier, 1, 0);
-        modifierajouterGrid.add(btnajouter, 2, 0);
-        //Choix question et reponse
-        GridPane questionreponseGrid = new GridPane();
+        labelajouter = new Label("Ajouter");
+        labelajouter.setAlignment(Pos.CENTER);
         
-        Label question_label = new Label("Question");
-        questionreponseGrid.add(question_label, 0, 1);
+        questionreponsegridajoutergrid = new GridPane();
+        question_label_ajouter = new Label("Question");
+        questionajoutertext = new TextField();
+        reponse_label_ajouter = new Label("Réponse");
+        reponseajoutertext = new TextField();
+        questionreponsegridajoutergrid.add(question_label_ajouter, 0, 1);
+        questionreponsegridajoutergrid.add(reponse_label_ajouter, 0, 2);
+        questionreponsegridajoutergrid.add(questionajoutertext, 1, 1);
+        questionreponsegridajoutergrid.add(reponseajoutertext, 1, 2);
         
-        Label reponse_label = new Label("Reponse");
-        questionreponseGrid.add(reponse_label, 0, 2);       
+        annuleretenregisterajoutergrid = new GridPane();
+        annulerajouter = new Button("Annuler");
+        enregistrerajouter = new Button("Enregistrer");
+        annuleretenregisterajoutergrid.add(annulerajouter, 0, 1);
+        annuleretenregisterajoutergrid.add(enregistrerajouter, 1, 1);
         
-        TextField questiontext = new TextField();
-        questionreponseGrid.add(questiontext, 1, 1);
+        ajoutervbox.getChildren().add(labelajouter);
+        ajoutervbox.getChildren().add(questionreponsegridajoutergrid);
+        ajoutervbox.getChildren().add(annuleretenregisterajoutergrid);
         
-        TextField reponseText = new TextField();
-        questionreponseGrid.add(reponseText, 1, 2);
+        ajoutervbox.setSpacing(20);
+           
+//=============================== PANNEAU MODIFIER ==============================================================        
+        //Construction de l'affichage du panneau qui permet de modifier une question
+        modifiervbox = new VBox();
         
-        //Choix bouton enregistrer
-        Button btnenregistrer = new Button("Enregistrer");
+        labelmodifier = new Label("Modifier");
+        labelmodifier.setAlignment(Pos.CENTER);
         
-        this.setSpacing(30);
+        questionreponsemodifiergrid = new GridPane();
+        question_label_modifier = new Label("Question");
+        questionmodifiertext = new TextField();
+        reponse_label_modifier = new Label("Réponse");
+        reponsemodifiertext = new TextField();
+        
+        questionreponsemodifiergrid.add(question_label_modifier, 0, 1);
+        questionreponsemodifiergrid.add(reponse_label_modifier, 0, 2);
+        questionreponsemodifiergrid.add(questionmodifiertext, 1, 1);
+        questionreponsemodifiergrid.add(reponsemodifiertext, 1,2);
+        
+        annuleretenregistrermodifiergrid = new GridPane();
+        annulermodifier = new Button("Annuler");
+        enregistrermodifier = new Button("Enregistrer");
+        annuleretenregistrermodifiergrid.add(annulermodifier, 0, 1);
+        annuleretenregistrermodifiergrid.add(enregistrermodifier,1, 1);
+        
+        modifiervbox.getChildren().add(labelmodifier);
+        modifiervbox.getChildren().add(questionreponsemodifiergrid);
+        modifiervbox.getChildren().add(annuleretenregistrermodifiergrid);
+        
+        modifiervbox.setSpacing(20);
+        
+        modifieretajoutergrid.add(ajoutervbox, 0, 1);
+        modifieretajoutergrid.add(modifiervbox, 1, 1);
+        
+        modifieretajoutergrid.setHgap(30);
+        
+        btnsedeconnecter = new Button("Se déconnecter");
+ 
+        this.setSpacing(50);
 
-        this.getChildren().add(radioGrid);
-        this.getChildren().add(modifierajouterGrid);
-        this.getChildren().add(questionreponseGrid);
-        this.getChildren().add(btnenregistrer);
+        this.getChildren().add(radiovbox);
+        this.getChildren().add(modifieretajoutergrid);
+        this.getChildren().add(btnsedeconnecter);
         
-        radioGrid.setAlignment(Pos.TOP_CENTER);
-        modifierajouterGrid.setAlignment(Pos.TOP_CENTER);
-        questionreponseGrid.setAlignment(Pos.TOP_CENTER);
-        btnenregistrer.setAlignment(Pos.TOP_CENTER);
-        
+        radiovbox.setAlignment(Pos.TOP_CENTER);
+        modifieretajoutergrid.setAlignment(Pos.TOP_CENTER);
+        btnsedeconnecter.setAlignment(Pos.TOP_CENTER);
         
         this.setAlignment(Pos.CENTER);
     }
